978f5f36e54b31041206b9fa8caf317bee4d7ff5
saskiamaulansyah <saskiamaulansyah@gmail.com>
2021-06-18 21:10:10 +0700

    initial commit

---
+++ src/pos_restoran/Pos_restoran.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pos_restoran;
+
+/**
+ *
+ * @author A455L
+ */
+public class Pos_restoran {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
